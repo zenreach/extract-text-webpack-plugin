24873ccfc0586d0c0dff1069f6e08ebb060a871a
ceineke <chris@greenfieldlabs.ca>
2018-10-22 14:34:27 -0400

    Patch out error that occurs when running under bazel.

--- loader.js
+++ loader.js
@@ -17,12 +17,7 @@ module.exports.pitch = function(request) {
 	var query = loaderUtils.parseQuery(this.query);
 	this.addDependency(this.resourcePath);
 	// We already in child compiler, return empty bundle
-	if(this[__dirname] === undefined) {
-		throw new Error(
-			'"extract-text-webpack-plugin" loader is used without the corresponding plugin, ' +
-			'refer to https://github.com/webpack/extract-text-webpack-plugin for the usage example'
-		);
-	} else if(this[__dirname] === false) {
+	if(!this[__dirname]) {
 		return "";
 	} else if(this[__dirname](null, query)) {
 		if(query.omit) {
